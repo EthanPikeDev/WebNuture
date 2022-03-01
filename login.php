23e95b9ce47ab1d2854343e2e206e1d2b16ff813
ethanpikedev <ethanpikedev@gmail.com>
2022-03-01 15:59:52 +0200

    added login page

---
+++ login.php
@@ -0,0 +1,18 @@
+<?php
+//start php session login form 
+session_start(); /* Starts the session */
+/* Check Login form submitted */if(isset($_POST['Submit'])){
+/* Define username and associated password array */$logins = array('Ethan' => '123456','username1' => 'password1','username2' => 'password2');
+
+/* Check and assign submitted Username and Password to new variable */$Username = isset($_POST['Username']) ? $_POST['Username'] : '';
+$Password = isset($_POST['Password']) ? $_POST['Password'] : '';
+
+/* Check Username and Password existence in defined array */if (isset($logins[$Username]) && $logins[$Username] == $Password){
+/* Success: Set session variables and redirect to Protected page  */$_SESSION['UserData']['Username']=$logins[$Username];
+header("location:account-page.php");
+exit;
+} else {
+/*Unsuccessful attempt: Set error message */$msg="<span style='color:red'>Invalid Login Details</span>";
+}
+}
+?>
